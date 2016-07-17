481780989ec778c79028d668c386d0d0ac2fb6a7
Alexander Black Jr <alexblackjr@users.noreply.github.com>
2016-07-17 06:30:43 -0400

    position and timing changes updated

--- js/app.js
+++ js/app.js
@@ -1,20 +1,28 @@
 
 $(document).ready(function(){
     'use strict';
-    $('#bigWave').delay(1500).css('visibility', 'visible').animate({opacity: 1.0, top: '-300px'}, 6500);  
-    $('#wakeUp').delay(4500).css('visibility', 'visible').animate({opacity: 1.0, top: '50px'}, 3000);
-    $('#wakeUp').fadeOut(2000);
-    $('#bigWave').delay(1500).animate({opacity:0.3}, 1500);
-    $('#alive').delay(11000).css('visibility', 'visible').animate({opacity: 1.0, top: '50px'}, 1200); 
-    $('#nixon').delay(11800).css('visibility', 'visible').animate({opacity: 1.0, top: '50px'}, 1000); 
-    $('#nixon').delay(1000).fadeOut(1000);
-    $('#alive').delay(1800).fadeOut(1000);
-    $('#bigWave').delay(4250).fadeOut(1500);
-    $('#backDrop').delay(16000).css('visibility', 'visible').animate({opacity: 1.0}, 1000); 
-    $('#watch').delay(17000).css('visibility', 'visible').animate({opacity: 1.0}, 1000);
-    $('#diplo').delay(17500).css('visibility', 'visible').animate({opacity: 1.0}, 1000);
-    $('#shop').delay(18000).css('visibility', 'visible').animate({opacity: 1.0}, 1000);
-    $('#logo').delay(18750).css('visibility', 'visible').animate({opacity: 1.0, top: '50px'}, 1000); 
+    $('#bigWave').delay(100).css('visibility', 'visible')
+        .animate({opacity: 1.0, top: '-240px'}, 6500)
+        .delay(100).animate({opacity:0.3}, 1500)
+        .delay(3500).fadeOut(1400);
+    $('#wakeUp').delay(2700).css('visibility', 'visible')
+        .animate({opacity: 1.0, top: '30px'}, 3000).fadeOut(800);
+    $('#alive').delay(8500).css('visibility', 'visible')
+        .animate({opacity: 1.0, top: '5px'}, 1000)
+        .delay(1500).fadeOut(800);
+    $('#nixon').delay(9500).css('visibility', 'visible')
+        .animate({opacity: 1.0, top: '17px'}, 1000)
+        .delay(700).fadeOut(700);
+    $('#backDrop').delay(16000).css('visibility', 'visible')
+        .animate({opacity: 1.0}, 1000);
+    $('#watch').delay(13500).css('visibility', 'visible')
+        .animate({opacity: 1.0}, 1000);
+    $('#diplo').delay(14000).css('visibility', 'visible')
+        .animate({opacity: 1.0}, 1000);
+    $('#shop').delay(14500).css('visibility', 'visible')
+        .animate({opacity: 1.0}, 1000);
+    $('#logo').delay(15000).css('visibility', 'visible')
+        .animate({opacity: 1.0, top: '1px'}, 800);
     
      $("#collapse").on("click", function(){
         $("#collapse").css('visibility', 'hidden');  
@@ -24,24 +32,20 @@ $(document).ready(function(){
     });
     
     $("#expand").on("click", function(){
-       
         $("#expand").css('visibility','hidden');
         $("img").remove(".smallView");
         $("#collapse").css('visibility', 'visible');
-         
-      
-      
-        $(".bottom").before('<img id="backDrop" class="bigView" src="img/backDrop.png"/>');
-        $('#backDrop').css('visibility', 'visible').animate({opacity: 1.0}, 1000); 
-        $(".bottom").before('<img id="watch" class="bigView" src="img/watch.png"/>');
+        $(".bottom").before('<img id="backDrop" class="bigView" src="img/backDrop.png"/>')
+            .before('<img id="watch" class="bigView" src="img/watch.png"/>')
+            .before('<img id="diplo" class="bigView" src="img/diplo.png"/>')
+            .before('<img id="shop" class="bigView"  src="img/shopButton.png"/>')
+            .before('<img id="logo" class="bigView" src="img/nixon.png"/>')
+            .before('<img class="smallView" src="img/smallAd.png"/>');
+        $('#backDrop').css('visibility', 'visible').animate({opacity: 1.0}, 1000);
         $('#watch').delay(1000).css('visibility', 'visible').animate({opacity: 1.0}, 1000);
-        $(".bottom").before('<img id="diplo" class="bigView" src="img/diplo.png"/>');
         $('#diplo').delay(1750).css('visibility', 'visible').animate({opacity: 1.0}, 1000);
-        $(".bottom").before('<img id="shop" class="bigView"  src="img/shopButton.png"/>');
         $("#shop").delay(2500).css('visibility', 'visible').animate({opacity: 1.0}, 1000);
-        $(".bottom").before('<img id="logo" class="bigView" src="img/nixon.png"/>');
-        $('#logo').delay(3000).css('visibility', 'visible').animate({opacity: 1.0, top: '50px'}, 1000);
-        $(".bottom").before('<img class="smallView" src="img/smallAd.png"/>');            
+        $('#logo').delay(3000).css('visibility', 'visible').animate({opacity: 1.0, top: '1px'}, 1000);
         $("#innerFrame").css('background','#000000');
  });
     
